80dbe71c7f9ecb5ecf5dc3f2342216194d95da37
moellsherbiny <moellsherbiny@gmail.com>
2025-04-03 16:40:17 +0200

    ad

--- src/app/api/courses/[course_id]/enroll/route.ts
+++ src/app/api/courses/[course_id]/enroll/route.ts
@@ -1,28 +1,29 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function POST(
-  request: Request,
-  { params }: { params: { course_id: string } }
-) {
+export async function POST(request: Request) {
   try {
-    const { course_id } = await params;
-    const { studentId } = await request.json();
-    if (!studentId) {
-      return NextResponse.json({ error: "Missing studentId" }, { status: 400 });
+    const { studentId, courseId } = await request.json();
+    if (!studentId || !courseId) {
+      return NextResponse.json(
+        { error: "Missing studentId or course_id" },
+        { status: 400 }
+      );
     }
-    
-  
-    
-    const enrollment = await prisma.enrollment.create(
-    {data:{
-      studentId,
-      courseId:course_id
-    }})
-      
+
+    const enrollment = await prisma.enrollment.create({
+      data: {
+        studentId,
+        courseId,
+      },
+    });
+
     return NextResponse.json({ enrollment });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
   }
 }
