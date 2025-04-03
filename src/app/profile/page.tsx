6c677300f110a727ced191ee25f72cb75a02083d
moellsherbiny <moellsherbiny@gmail.com>
2025-04-03 17:42:15 +0200

    a

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,13 @@
 import DashboardLayout from "@/components/Dashboard";
 import { getUserData } from "@/lib/getUserData";
 import ProfileForm from "@/components/ProfileForm";
+import { redirect } from "next/navigation";
 
 export default async function ProfilePage() {
   const user = await getUserData();
-  
+  if (!user || "error" in user)
+    redirect("/")
+
   return (
     <DashboardLayout>
       <div className="flex items-center justify-center min-h-screen p-6">
