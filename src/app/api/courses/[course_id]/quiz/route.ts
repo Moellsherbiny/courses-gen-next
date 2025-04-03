dc14d67649e7630140803ab859d4d880977d641d
moellsherbiny <moellsherbiny@gmail.com>
2025-04-03 17:02:10 +0200

    a

--- src/app/api/courses/[course_id]/quiz/route.ts
+++ src/app/api/courses/[course_id]/quiz/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
-type Params = {
+type Params = Promise<{
   course_id: string;
-}
-export async function GET(request: Request, data : {params:Params}) {
+}>;
+export async function GET(request: Request, data: { params: Params }) {
   try {
-    const params = data.params;
-    const course_id = params.course_id;
+    const params = await data.params;
+    const course_id =  params.course_id;
     const quiz = await prisma.quiz.findFirst({
       where: { courseId: course_id },
       include: {
